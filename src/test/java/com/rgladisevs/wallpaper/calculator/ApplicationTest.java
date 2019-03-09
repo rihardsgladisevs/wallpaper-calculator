253ac5339a580e0547c4a6ce491e1694f892d63b
rg <rg@weissenrieder.com>
2019-03-09 17:30:18 +0200

    test on Application main method which proves that app runs

---
+++ src/test/java/com/rgladisevs/wallpaper/calculator/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.rgladisevs.wallpaper.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+        String[] args = {};
+        Application.main(args);
+    }
+}
